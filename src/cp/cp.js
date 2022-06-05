5348bbbdb4c3564dcf8aefd32bfc7d3b4979c7d5
4workacc <58693140+4workacc@users.noreply.github.com>
2022-06-05 15:05:08 +0300

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,10 @@
+import { exec, execFileSync } from "child_process";
+
 export const spawnChildProcess = async (args) => {
     // Write your code here
+    const child = exec(`node "./files/script.js" ${args}`, (err, stdout, stderr) => {
+        stdout.write( stdout );
+    })
 };
+
+spawnChildProcess();
